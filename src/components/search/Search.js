dc93349b786781cb0b536f3f1a44f8dabbd3b646
niszeto <niszeto@gmail.com>
2019-07-17 19:23:56 -0700

    cleaned up text correcting functions and moved them into utils file

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,11 +1,11 @@
 import React, { useState, useContext } from "react";
 import ImageContext from "../../context/image/imageContext";
-// import words from "../../assets/words_dictionary";
 import styles from "./Search.module.css";
+import { correctWord } from "../../utils/utils";
 
 const Search = props => {
   const imageContext = useContext(ImageContext);
-  const { getImagesData } = imageContext;
+  const { getImagesData, setLoading, isLoading } = imageContext;
 
   const [searchText, setSearchText] = useState("");
 
@@ -15,97 +15,14 @@ const Search = props => {
 
   const search = event => {
     event.preventDefault();
-    if (searchText !== "") {
-      getImagesData(searchText);
+    if (searchText !== "" || isLoading) {
+      setLoading();
+
+      let word = correctWord(searchText);
+      setSearchText(word);
+      getImagesData(word);
     }
-
-    event.stopPropagation();
   };
-
-  const addOneCharacterToWord = word => {
-    const alphabet = "abcdefghijklmnopqrstuvwxyz";
-
-    word = word
-      .toLowerCase()
-      .replace(/[^a-z]/gi, "")
-      .split("");
-    let results = [];
-
-    for (let i = 0; i <= word.length; i++) {
-      for (let j = 0; j < alphabet.length; j++) {
-        let newWord = word.slice();
-        newWord.splice(i, 0, alphabet[j]);
-        results.push(newWord.join(""));
-      }
-    }
-
-    return results;
-  };
-
-  // console.log(addOneCharacterToWord("Ceku"));
-
-  const removeOneCharacterFromWord = word => {
-    word = word
-      .toLowerCase()
-      .replace(/[^a-z]/gi, "")
-      .split("");
-    let results = [];
-
-    if (word.length > 1) {
-      for (let i = 0; i < word.length; i++) {
-        let newWord = word.slice();
-        newWord.splice(i, 1);
-        results.push(newWord.join(""));
-      }
-    }
-
-    return results;
-  };
-
-  // console.log(removeOneCharacterFromWord("Ceku"));
-
-  const swapLettersInWord = word => {
-    word = word
-      .toLowerCase()
-      .replace(/[^a-z]/gi, "")
-      .split("");
-    let results = [];
-
-    if (word.length > 1) {
-      for (let i = 0; i < word.length; i++) {
-        let newWord = word.slice();
-        let letterToBeSwapped = newWord.splice(i, 1);
-        newWord.splice(i + 1, 0, letterToBeSwapped[0]);
-        results.push(newWord.join(""));
-      }
-    }
-
-    return results;
-  };
-
-  // console.log(swapLettersInWord("Ceku"));
-
-  const replaceLetterInWord = word => {
-    const alphabet = "abcdefghijklmnopqrstuvwxyz";
-
-    word = word
-      .toLowerCase()
-      .replace(/[^a-z]/gi, "")
-      .split("");
-    let results = [];
-
-    for (let i = 0; i < word.length; i++) {
-      for (let j = 0; j < alphabet.length; j++) {
-        let newWord = word.slice();
-        newWord[i] = alphabet[j];
-        results.push(newWord.join(""));
-      }
-    }
-
-    return results;
-  };
-
-  // console.log(replaceLetterInWord("Ceku"));
 
   return (
     <form onSubmit={search}>
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,2 +1,139 @@
-// const words = require("../assets/words_dictionary");
-// console.log(word.toLowerCase().replace(/[^a-z]/gi, ''));
+import words from "../assets/words_dictionary";
+
+export const addOneLetterToWord = word => {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  let results = [];
+
+  for (let i = 0; i <= word.length; i++) {
+    for (let j = 0; j < alphabet.length; j++) {
+      let newWord = word.slice();
+      newWord.splice(i, 0, alphabet[j]);
+      results.push(newWord.join(""));
+    }
+  }
+
+  return results;
+};
+
+export const removeOneLetterFromWord = word => {
+  let results = [];
+
+  if (word.length > 1) {
+    for (let i = 0; i < word.length; i++) {
+      let newWord = word.slice();
+      newWord.splice(i, 1);
+      results.push(newWord.join(""));
+    }
+  }
+
+  return results;
+};
+
+export const swapLettersInWord = word => {
+  let results = [];
+
+  if (word.length > 1) {
+    for (let i = 0; i < word.length; i++) {
+      let newWord = word.slice();
+      let letterToBeSwapped = newWord.splice(i, 1);
+      newWord.splice(i + 1, 0, letterToBeSwapped[0]);
+      results.push(newWord.join(""));
+    }
+  }
+
+  return results;
+};
+
+export const replaceLetterInWord = word => {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  let results = [];
+
+  for (let i = 0; i < word.length; i++) {
+    for (let j = 0; j < alphabet.length; j++) {
+      let newWord = word.slice();
+      newWord[i] = alphabet[j];
+      results.push(newWord.join(""));
+    }
+  }
+
+  return results;
+};
+
+export const oneDistanceFromWord = word => {
+  let results = [];
+
+  word = word
+    .toLowerCase()
+    .replace(/[^a-z]/gi, "")
+    .split("");
+
+  results = results.concat(swapLettersInWord(word));
+  results = results.concat(replaceLetterInWord(word));
+  results = results.concat(addOneLetterToWord(word));
+  results = results.concat(removeOneLetterFromWord(word));
+
+  return results;
+};
+
+export const correctWord = word => {
+  word = word.toLowerCase().replace(/[^a-z]/gi, "");
+  if (words[word]) {
+    return word;
+  }
+
+  word = word.split("");
+
+  let wordsOneDistanceAway = [];
+
+  let swappedLetterWords = swapLettersInWord(word);
+  for (let swappedLetterWord of swappedLetterWords) {
+    if (words[swappedLetterWord]) {
+      return swappedLetterWord;
+    }
+  }
+  wordsOneDistanceAway = wordsOneDistanceAway.concat(swappedLetterWords);
+
+  let replaceLetterWords = replaceLetterInWord(word);
+  for (let replaceLetterWord of replaceLetterWords) {
+    if (words[replaceLetterWord]) {
+      return replaceLetterWord;
+    }
+  }
+  wordsOneDistanceAway = wordsOneDistanceAway.concat(replaceLetterWords);
+
+  let addOneLetterWords = addOneLetterToWord(word);
+  for (let addOneLetterWord of addOneLetterWords) {
+    if (words[addOneLetterWord]) {
+      return addOneLetterWord;
+    }
+  }
+  wordsOneDistanceAway = wordsOneDistanceAway.concat(addOneLetterWords);
+
+  let removeOneLetterWords = removeOneLetterFromWord(word);
+  for (let removeOneLetterWord of removeOneLetterWords) {
+    if (words[removeOneLetterWord]) {
+      return removeOneLetterWord;
+    }
+  }
+  wordsOneDistanceAway = wordsOneDistanceAway.concat(removeOneLetterWords);
+
+  let wordsTwoDistanceAway = [];
+
+  wordsTwoDistanceAway = wordsTwoDistanceAway.concat(
+    oneDistanceFromWord(wordsOneDistanceAway[0])
+  );
+
+  for (let i = 0; i < wordsOneDistanceAway.length; i++) {
+    wordsTwoDistanceAway = wordsTwoDistanceAway.concat(
+      oneDistanceFromWord(wordsOneDistanceAway[i])
+    );
+  }
+
+  for (let wordTwoDistanceAway of wordsTwoDistanceAway) {
+    if (words[wordTwoDistanceAway]) {
+      return wordTwoDistanceAway;
+    }
+  }
+
+  return word.join("");
+};

d68babd6fef732f7a6ff27e12ff0f0dfbfa67720
niszeto <niszeto@gmail.com>
2019-07-17 19:40:36 -0700

    moved non global styling out of app.css and when no photos are found display text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,15 +4,19 @@ import ImageModal from "../components/modal/ImageModal";
 import Search from "../components/search/Search";
 import Spinner from "../components/layout/Spinner";
 import GalleryList from "../components/gallery/GalleryList";
+import styles from "./Home.module.css";
 
 const Home = props => {
   const imageContext = useContext(ImageContext);
-  const { isLoading } = imageContext;
+  const { isLoading, imagesData } = imageContext;
 
   return (
-    <div className="container">
+    <div className={styles.container}>
       <ImageModal />
       <Search />
+      {imagesData.length === 0 && (
+        <h1 className={styles.text}>Search to display photos</h1>
+      )}
       {isLoading ? <Spinner /> : <GalleryList />}
     </div>
   );
